a4f7600aa1beeb43c06c4cec37a02c284e322622
NeenuAVarghese <neenuavarghese@gmail.com>
2016-04-16 18:54:04 -0700

    Defined Custom IDs

--- scripts/quickhub-app.js
+++ scripts/quickhub-app.js
@@ -1,17 +1,44 @@
 'use strict';
 
+var qh = {
+	landpage:{
+		section:{
+			navbar: "#qhHeader",
+			drawer: "#qhDrawer",
+			content: "#qhContent"
+		},
+		tab1SearchCard:{
+			handle: "#qhTab1Card",
+			field: "#qhTab1Search"
+		},
+		tab2SearchCard:{
+			handle: "#qhTab2Card",
+			field: "#qhTab2Search"
+		}
+	},
+	pullUserDataCardTab1:{
+		handle: "#qhTab1getUserDetails",
+		field:{
+			textBox: "#qhTab1SearchUserText",
+			searchButton: "qhTab1SearchUserButton",
+			closeButton: "#qhTab1Close"
+		}
+	}	
+};
 
-        var dialog = document.querySelector('#modal-example');
-        var closeButton = dialog.querySelector('.close');
-        var showButton = document.querySelector('#show-modal-example');
-        if (! dialog.showModal) {
+var dialog = document.querySelector("#qhTab1getUserDetails");
+
+function init(){
+	if (! dialog.showModal) {
             dialogPolyfill.registerDialog(dialog);
         }
-        var closeClickHandler = function(event) {
-            dialog.close();
-        };
-        var showClickHandler = function(event) {
-            dialog.showModal();
-        };
-        showButton.addEventListener('click', showClickHandler);
-        closeButton.addEventListener('click', closeClickHandler);
+}
+$(qh.landpage.tab1SearchCard.field).on('click', function(){
+	dialog.showModal();
+	
+	$(qh.pullUserDataCardTab1.field.closeButton).on('click', function(){
+		dialog.close();
+	});
+});
+
+$(document).ready(init);
