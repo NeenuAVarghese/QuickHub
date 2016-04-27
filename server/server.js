8f92eaf4630463fa0198da062b381e8476b64738
NeenuAVarghese <neenuavarghese@gmail.com>
2016-04-27 02:02:46 -0700

    Handles server side code

--- server/server.js
+++ server/server.js
@@ -2,42 +2,42 @@
 /* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
 // Server-side code
 /* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
-{
-    "curly": true,
-    "eqeqeq": true,
-    "forin": true,
-    "immed": true,
-    "indent": 4,
-    "latedef": true,
-    "newcap": true,
-    "nonew": true,
-    "quotmark": "double",
-    "undef": true,
-    "unused": true,
-    "strict": true,
-    "trailing": true,
-    "node": true
-}
+/*{
+    "curly" : true,
+    "eqeqeq" : true,
+    "forin" : true,
+    "immed" : true,
+    "indent" : 4,
+    "latedef" : true,
+    "newcap" : true,
+    "nonew" : true,
+    "quotmark" : "double",
+    "undef" : true,
+    "unused" : true,
+    "strict" : true,
+    "trailing" : true,
+    "node" : true
+}*/
 
 var express = require("express");
-var Promise = require('promise');
 var Trending = require("github-trend");
-var client = new Trending.Client();
+var scraper = new Trending.Scraper();
 
 var app = express();
 app.use(express.static("./"));
-var qhTrends = [];
+
 
 
 app.get('/trending', function(req, res) {
+	var qhTrends = [];
     // Empty string means 'all languages' 
-    client.fetchTrending("").then(function(repos) {
-        for (var i = 0; i < 10; i++) {
+    scraper.scrapeTrendingRepos("").then(function(repos) {
+        for (var i = 0; i < 5; i++) {
             var repo = repos[i];
             qhTrends.push({
                 repoName: repo.name,
                 repoUrl: repo.html_url,
-                repoOwner: repo.owner.login,
+                repoOwner: repo.owner,
                 repoDescription: repo.description
             });
         }
