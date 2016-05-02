436ffaacdf5bc4cc2626f5403620bc02f484f272
NeenuAVarghese <neenuavarghese@gmail.com>
2016-05-01 20:39:06 -0700

    Added model to the db

---
+++ server/model/users.js
@@ -0,0 +1,21 @@
+
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+
+"use strict";
+var  mongoose = require("mongoose");
+
+// Mongoose model for todos
+var UserSchema = mongoose.Schema({
+    name: {type: String, required: true, index: {unique: true}},
+    password: {type: String, required: true},
+    itemsPinned: [ String ]
+});
+
+var User = mongoose.model("User", UserSchema);
+
+//Exporting model
+module.exports = User;
